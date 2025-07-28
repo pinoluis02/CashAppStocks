3655ed06d3b474debff0524a293121ee1cf0bb4c
Luis Perez <pinoluis02@gmail.com>
2025-07-28 18:34:59 -0500

    Added computed properties to StockVieWModel

--- CashAppStocks/ViewModels/StockViewModel.swift
+++ CashAppStocks/ViewModels/StockViewModel.swift
@@ -14,4 +14,33 @@ class StockViewModel {
     init(stock: Stock) {
         self.stock = stock
     }
+    
+    var displayName: String {
+        return self.stock.name
+    }
+    
+    var displaySymbol: String {
+        return self.stock.ticker
+    }
+    
+    var displayPrice: String {
+        let price = Double(stock.currentPriceCents) / 100
+        return String(format: "%.2f", price)
+    }
+    
+    var displayQuantity: String {
+        guard let quantity = stock.quantity else {
+            return "Qty: N/A"
+        }
+        return "Qty: \(quantity)"
+    }
+    
+    var formattedDate: String {
+        let date = Date(timeIntervalSince1970: TimeInterval(stock.currentPriceTimestamp))
+        
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .short
+        return formatter.string(from: date)
+    }
 }
---
+++ CashAppStocks/Views/StockListViewController.swift
@@ -0,0 +1,91 @@
+//
+//  StockListViewController.swift
+//  CashAppStocks
+//
+//  Created by Luis Perez on 7/28/25.
+//
+
+import UIKit
+
+class StockListViewController: UIViewController {
+    
+    private let viewModel: StockListViewModel
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    
+    init(viewModel: StockListViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupUI()
+        self.configureTableView()
+        self.bindViewModel()
+        self.viewModel.loadStocks()
+    }
+    
+    func setupUI() {
+        self.title = "Stocks"
+        self.view.backgroundColor = .systemBackground
+        self.view.addSubview(self.tableView)
+    }
+    
+    func configureTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "StockCell")
+        tableView.delegate = self
+        tableView.dataSource = self
+        NSLayoutConstraint.activate([
+            self.tableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            self.tableView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            self.tableView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+            self.tableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    func bindViewModel() {
+        self.viewModel.onStateChanged = { state in
+            switch state {
+            case .loading:
+                break
+            case .loaded:
+                self.tableView.reloadData()
+            case .empty:
+                break
+            case .error(let error):
+                break
+            }
+        }
+    }
+    
+}
+
+extension StockListViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.viewModel.stocks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = self.viewModel.stocks[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StockCell", for: indexPath)
+        
+        var config = cell.defaultContentConfiguration()
+        config.text = item.displayName
+        config.secondaryText = item.displaySymbol
+        cell.contentConfiguration = config
+        
+        return cell
+    }
+}
